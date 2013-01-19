cdd339307c039f4b020493751b8c865cc7ce94a6
dhleong <falcone88@gmail.com>
2013-01-19 11:02:02 -0500

    Move the "version" error into OnConnectedListener with the other errors

--- src/net/dhleong/acl/ArtemisNetworkInterface.java
+++ src/net/dhleong/acl/ArtemisNetworkInterface.java
@@ -5,8 +5,6 @@ public interface ArtemisNetworkInterface {
     /** The Artemis server version we support */
     public static final float TARGET_VERSION = 1.661f;
     
-    public static final int ERROR_VERSION = 42;
-
     public void addOnPacketListener(OnPacketListener listener);
 
     public void send(ArtemisPacket pkt);

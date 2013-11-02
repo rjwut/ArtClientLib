6e9d14e1364f0902564a2d437d9cdf18f10165e3
Robert J. Walker <rjw@walkertribe.com>
2013-11-01 23:05:42 -0600

    New branch will support only 2.0 and later
    Changed supported version to only 2.0, as this branch will not support
    older version

--- src/net/dhleong/acl/ArtemisNetworkInterface.java
+++ src/net/dhleong/acl/ArtemisNetworkInterface.java
@@ -3,13 +3,11 @@ package net.dhleong.acl;
 public interface ArtemisNetworkInterface {
     
     /** The main Artemis server version we're targeting */
-    public static final float TARGET_VERSION = 1.700f;
+    public static final float TARGET_VERSION = 2.000f;
     
     /** The Artemis server versions we support */
     public static final float[] SUPPORTED_VERSIONS = new float[] {
-        TARGET_VERSION,
-        1.701f, // iOS servers
-        1.702f // new update
+        TARGET_VERSION
     };
     
     public void addOnPacketListener(OnPacketListener listener);

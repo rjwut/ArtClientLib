ffc401eb554e3ede3a92d31cad5e3f93b425181b
Robert J. Walker <rjw@walkertribe.com>
2013-12-14 14:44:32 -0700

    Renamed DiveRisePacket to ClimbDivePacket
    This is to be more consistent with Artemis terminology

--- src/net/dhleong/acl/net/helm/ClimbDivePacket.java
+++ src/net/dhleong/acl/net/helm/ClimbDivePacket.java
@@ -3,7 +3,7 @@ package net.dhleong.acl.net.helm;
 import net.dhleong.acl.net.BaseArtemisPacket;
 import net.dhleong.acl.net.PacketParser;
 
-public class DiveRisePacket extends BaseArtemisPacket {
+public class ClimbDivePacket extends BaseArtemisPacket {
     public static final int TYPE = 0x0351A5AC;
     private static final int UP = -1;
     private static final int DOWN = 1;
@@ -11,7 +11,7 @@ public class DiveRisePacket extends BaseArtemisPacket {
     /**
      * @param up True if you want to tilt the ship up, false to tilt it down.
      */
-    public DiveRisePacket(boolean up) {
+    public ClimbDivePacket(boolean up) {
         super(0x02, TYPE, new byte[12]);
         PacketParser.putLendInt(0x1b, mData);
         PacketParser.putLendInt(up ? UP : DOWN, mData, 4);

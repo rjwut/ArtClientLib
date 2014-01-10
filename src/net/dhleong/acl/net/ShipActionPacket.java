4d67d871d08c5a57d04455b1d660f93d9512b3b7
Robert J. Walker <rjw@walkertribe.com>
2014-01-09 21:47:05 -0700

    Added key capture packets
    KeyCaptureTogglePacket, KeystrokePacket

--- src/net/dhleong/acl/net/ShipActionPacket.java
+++ src/net/dhleong/acl/net/ShipActionPacket.java
@@ -12,29 +12,30 @@ import net.dhleong.acl.enums.ConnectionType;
 public abstract class ShipActionPacket extends BaseArtemisPacket {
     public static final int TYPE = ArtemisPacket.SHIP_ACTION_TYPE;
     
-    protected static final int TYPE_WARPSPEED          = 0x00; 
+    protected static final int TYPE_WARPSPEED          = 0x00;
     protected static final int TYPE_MAINSCREEN         = 0x01;
     protected static final int TYPE_SET_TARGET         = 0x02;
     protected static final int TYPE_TOGGLE_AUTO_BEAMS  = 0x03;
-    
     protected static final int TYPE_TOGGLE_SHIELDS     = 0x04;
+
     protected static final int TYPE_REQUEST_DOCK       = 0x07;
-    
-    protected static final int TYPE_FIRE_TUBE          = 0x08; 
-    protected static final int TYPE_UNLOAD_TUBE        = 0x09; 
-    protected static final int TYPE_TOGGLE_REDALERT    = 0x0a; 
-    protected static final int TYPE_SET_BEAMFREQ       = 0x0b; 
-    protected static final int TYPE_AUTO_DAMCON        = 0x0c; 
-    protected static final int TYPE_SET_SHIP           = 0x0d; 
-    protected static final int TYPE_SET_STATION        = 0x0e; 
-    protected static final int TYPE_READY              = 0x0f; 
-    
+    protected static final int TYPE_FIRE_TUBE          = 0x08;
+    protected static final int TYPE_UNLOAD_TUBE        = 0x09;
+    protected static final int TYPE_TOGGLE_REDALERT    = 0x0a;
+    protected static final int TYPE_SET_BEAMFREQ       = 0x0b;
+    protected static final int TYPE_AUTO_DAMCON        = 0x0c;
+    protected static final int TYPE_SET_SHIP           = 0x0d;
+    protected static final int TYPE_SET_STATION        = 0x0e;
+    protected static final int TYPE_READY              = 0x0f;
     protected static final int TYPE_SCI_SELECT         = 0x10; 
-    protected static final int TYPE_CAPTAIN_SELECT     = 0x11; 
-    protected static final int TYPE_SCI_SCAN           = 0x13; 
-    protected static final int TYPE_SHIP_SETUP         = 0x16; 
-    
-    protected static final int TYPE_REVERSE_ENGINES    = 0x18; 
+    protected static final int TYPE_CAPTAIN_SELECT     = 0x11;
+
+    protected static final int TYPE_SCI_SCAN           = 0x13;
+    protected static final int TYPE_KEYSTROKE          = 0x14;
+
+    protected static final int TYPE_SHIP_SETUP         = 0x16;
+
+    protected static final int TYPE_REVERSE_ENGINES    = 0x18;
     protected static final int TYPE_READY2             = 0x19;
     protected static final int TYPE_TOGGLE_PERSPECTIVE = 0x1a;
 

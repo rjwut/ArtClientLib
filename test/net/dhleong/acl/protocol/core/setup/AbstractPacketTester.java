26a95bdf5a1fc33be7d1f4067f09b32e1ee0716b
Robert J. Walker <rjw@walkertribe.com>
2014-07-14 11:40:49 -0600

    Testing utilities updates
    Added Debugger support, TestPacketFile can now test for identical
    read/write of packets

--- test/net/dhleong/acl/protocol/core/setup/AbstractPacketTester.java
+++ test/net/dhleong/acl/protocol/core/setup/AbstractPacketTester.java
@@ -3,44 +3,44 @@ package net.dhleong.acl.protocol.core.setup;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.Assert;
 
 import net.dhleong.acl.enums.ConnectionType;
+import net.dhleong.acl.iface.BaseDebugger;
+import net.dhleong.acl.iface.Debugger;
+import net.dhleong.acl.iface.OutputStreamDebugger;
 import net.dhleong.acl.iface.PacketReader;
 import net.dhleong.acl.iface.PacketWriter;
 import net.dhleong.acl.protocol.ArtemisPacket;
 import net.dhleong.acl.protocol.ArtemisPacketException;
 import net.dhleong.acl.protocol.TestPacketFile;
-import net.dhleong.acl.util.TextUtil;
 
 public abstract class AbstractPacketTester<T extends ArtemisPacket> {
 	private static final boolean DEBUG = ManagementFactory.getRuntimeMXBean().getInputArguments().toString().indexOf("jdwp") >= 0;
 
 	protected abstract void testPackets(List<T> packets);
 
+	private Debugger debugger = DEBUG ? new OutputStreamDebugger() : new BaseDebugger();
+
 	protected void execute(String resourcePath, ConnectionType type, int packetCount) {
 		try {
 			List<T> list = new ArrayList<T>(packetCount);
-			TestPacketFile file = new TestPacketFile(resourcePath);
-	
+			URL url = TestPacketFile.class.getResource(resourcePath);
+			TestPacketFile file = new TestPacketFile(url);
+
 			if (DEBUG) {
 				System.out.println("### " + resourcePath + " ###");
-				System.out.println(getClass().getSimpleName() + " [R]: " + file);
 			}
-	
+
 			PacketReader reader = file.toPacketReader(type);
 	
 			for (int i = 0; i < packetCount; i++) {
-				T pkt = (T) reader.readPacket();
+				T pkt = (T) reader.readPacket(debugger);
 				Assert.assertNotNull(pkt);
-	
-				if (DEBUG) {
-					System.out.println(pkt);
-				}
-	
 				list.add(pkt);
 			}
 
@@ -50,13 +50,9 @@ public abstract class AbstractPacketTester<T extends ArtemisPacket> {
 			PacketWriter writer = new PacketWriter(baos);
 	
 			for (T pkt : list) {
-				pkt.writeTo(writer);
+				pkt.writeTo(writer, debugger);
 			}
-	
-			if (DEBUG) {
-				System.out.println(TextUtil.byteArrayToHexString(baos.toByteArray()));
-			}
-	
+
 			Assert.assertTrue(file.matches(baos));
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);

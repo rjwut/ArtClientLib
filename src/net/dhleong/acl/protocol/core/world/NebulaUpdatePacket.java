a65e81203cc55ec0aba3a984c7e4e424b7c18af4
Robert J. Walker <rjw@walkertribe.com>
2014-07-27 21:52:15 -0600

    ArtemisNebula broken off from ArtemisGenericObject
    ArtemisNebula and NebulaUpdatePacket broken off from
    ArtemisGenericObject and GenericObjectUpdatePacket

--- src/net/dhleong/acl/protocol/core/world/NebulaUpdatePacket.java
+++ src/net/dhleong/acl/protocol/core/world/NebulaUpdatePacket.java
@@ -12,58 +12,45 @@ import net.dhleong.acl.iface.PacketWriter;
 import net.dhleong.acl.protocol.ArtemisPacket;
 import net.dhleong.acl.protocol.ArtemisPacketException;
 import net.dhleong.acl.protocol.BaseArtemisPacket;
-import net.dhleong.acl.world.ArtemisGenericObject;
+import net.dhleong.acl.world.ArtemisNebula;
 import net.dhleong.acl.world.ArtemisObject;
 
-/**
- * Update/create ArtemisGenericObjects
- * @author dhleong
- */
-public class GenericUpdatePacket extends BaseArtemisPacket implements ObjectUpdatingPacket {
-	private static ObjectType[] GENERIC_TYPES = {
-		ObjectType.MINE, ObjectType.ANOMALY, ObjectType.TORPEDO,
-		ObjectType.BLACK_HOLE, ObjectType.ASTEROID
-	};
-
-	private static final byte[] UNK_TORPEDO = { 0 };
-
+public class NebulaUpdatePacket extends BaseArtemisPacket implements ObjectUpdatingPacket {
 	public static void register(PacketFactoryRegistry registry) {
 		PacketFactory factory = new PacketFactory() {
 			@Override
 			public Class<? extends ArtemisPacket> getFactoryClass() {
-				return GenericUpdatePacket.class;
+				return NebulaUpdatePacket.class;
 			}
 
 			@Override
 			public ArtemisPacket build(PacketReader reader)
 					throws ArtemisPacketException {
-				return new GenericUpdatePacket(reader);
+				return new NebulaUpdatePacket(reader);
 			}
 		};
 
-		for (ObjectType objType : GENERIC_TYPES) {
-			registry.register(ConnectionType.SERVER, WORLD_TYPE,
-					objType.getId(), factory);
-		}
+		registry.register(ConnectionType.SERVER, WORLD_TYPE,
+				ObjectType.NEBULA.getId(), factory);
 	}
 
 	private enum Bit {
     	X,
     	Y,
     	Z,
-    	NAME,
-    	UNK_1_5,
-    	UNK_1_6,
+    	RED,
+    	GREEN,
+    	BLUE,
     	UNK_1_7,
     	UNK_1_8
     }
 
     private final List<ArtemisObject> mObjects = new ArrayList<ArtemisObject>();
 
-    private GenericUpdatePacket(PacketReader reader) {
+    private NebulaUpdatePacket(PacketReader reader) {
     	super(ConnectionType.SERVER, WORLD_TYPE);
         float x, y, z;
-        String name;
+        float r, g, b;
 
         while (reader.hasMore()) {
             reader.startObject(Bit.values());
@@ -76,24 +63,17 @@ public class GenericUpdatePacket extends BaseArtemisPacket implements ObjectUpda
             x = reader.readFloat(Bit.X, Float.MIN_VALUE);
             y = reader.readFloat(Bit.Y, Float.MIN_VALUE);
             z = reader.readFloat(Bit.Z, Float.MIN_VALUE);
-
-            if (type.isNamed()) {
-                name = reader.readString(Bit.NAME);
-            } else {
-                name = null;
-                reader.readObjectUnknown(Bit.NAME, 4);
-            }
-
-            reader.readObjectUnknown(Bit.UNK_1_5, 4);
-            reader.readObjectUnknown(Bit.UNK_1_6, 4);
+            r = reader.readFloat(Bit.RED, Float.MIN_VALUE);
+            g = reader.readFloat(Bit.GREEN, Float.MIN_VALUE);
+            b = reader.readFloat(Bit.BLUE, Float.MIN_VALUE);
             reader.readObjectUnknown(Bit.UNK_1_7, 4);
             reader.readObjectUnknown(Bit.UNK_1_8, 4);
 
-            final ArtemisGenericObject obj = new ArtemisGenericObject(
-            		reader.getObjectId(), name, type);
+            final ArtemisNebula obj = new ArtemisNebula(reader.getObjectId());
             obj.setX(x);
             obj.setY(y);
             obj.setZ(z);
+            obj.setARGB(0, r, g, b);
             obj.setUnknownProps(reader.getUnknownObjectProps());
             mObjects.add(obj);
         }
@@ -101,40 +81,25 @@ public class GenericUpdatePacket extends BaseArtemisPacket implements ObjectUpda
     	reader.skip(4);	// skip 0x00 terminator
     }
 
-    public GenericUpdatePacket() {
-    	super(ConnectionType.SERVER, WORLD_TYPE);
-    }
-
-    @Override
-    public List<ArtemisObject> getObjects() {
-        return mObjects;
-    }
+	@Override
+	public List<ArtemisObject> getObjects() {
+		return mObjects;
+	}
 
 	@Override
 	protected void writePayload(PacketWriter writer) {
 		Bit[] bits = Bit.values();
 
 		for (ArtemisObject obj : mObjects) {
-			ArtemisGenericObject gObj = (ArtemisGenericObject) obj;
-			ObjectType type = obj.getType();
+			ArtemisNebula nObj = (ArtemisNebula) obj;
 			writer.startObject(obj, bits);
 
-        	if (type == ObjectType.TORPEDO) {
-                writer.writeUnknown("UNK_TORPEDO", UNK_TORPEDO);
-            }
-
-        	writer	.writeFloat(Bit.X, gObj.getX(), Float.MIN_VALUE)
-					.writeFloat(Bit.Y, gObj.getY(), Float.MIN_VALUE)
-					.writeFloat(Bit.Z, gObj.getZ(), Float.MIN_VALUE);
-
-            if (obj.getType().isNamed()) {
-            	writer.writeString(Bit.NAME, gObj.getName());
-            } else {
-            	writer.writeUnknown(Bit.NAME);
-            }
-
-            writer	.writeUnknown(Bit.UNK_1_5)
-					.writeUnknown(Bit.UNK_1_6)
+        	writer	.writeFloat(Bit.X, nObj.getX(), Float.MIN_VALUE)
+					.writeFloat(Bit.Y, nObj.getY(), Float.MIN_VALUE)
+					.writeFloat(Bit.Z, nObj.getZ(), Float.MIN_VALUE)
+					.writeFloat(Bit.RED, nObj.getRed(), Float.MIN_VALUE)
+					.writeFloat(Bit.GREEN, nObj.getGreen(), Float.MIN_VALUE)
+					.writeFloat(Bit.BLUE, nObj.getBlue(), Float.MIN_VALUE)
 					.writeUnknown(Bit.UNK_1_7)
 					.writeUnknown(Bit.UNK_1_8)
 					.endObject();

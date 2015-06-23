b2d5e73b974e90d943f47ef3153d3fa9c66739d8
Robert J. Walker <rjw@walkertribe.com>
2015-06-22 18:52:44 -0600

    Additional documentation for BeamPort

--- src/net/dhleong/acl/vesseldata/BeamPort.java
+++ src/net/dhleong/acl/vesseldata/BeamPort.java
@@ -2,7 +2,8 @@ package net.dhleong.acl.vesseldata;
 
 /**
  * Describes a single beam port on a vessel. Corresponds to the <beam_port>
- * element in vesselData.xml.
+ * element in vesselData.xml. This class extends WeaponPort and adds an arcWidth
+ * property to define the beam weapon's firing arc.
  * @author rjwut
  */
 public class BeamPort extends WeaponPort {
